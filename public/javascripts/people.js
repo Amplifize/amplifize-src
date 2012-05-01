0cb7e3c747e8522cc37742a956a5f3f9d3d5d093
Melih Onvural <melih.onvural@gmail.com>
2012-05-01 14:53:38 -0700

    Fixed messaging how validation works on the follow page

--- public/javascripts/people.js
+++ public/javascripts/people.js
@@ -13,6 +13,14 @@ var loadManagePeople = function() {
 	// });
 };
 
+var customValidate = function(input) {
+	if(input.validity.typeMismatch){ 
+		input.setCustomValidity("Email not valid. Maybe there's an extra space?"); 
+	} else { 
+		input.setCustomValidity(""); 
+	}
+}
+
 $(document).ready(function() {
 	$("li#peopleNav.drawer ul").css("display", "block").css("visibility", "visible");
 });
