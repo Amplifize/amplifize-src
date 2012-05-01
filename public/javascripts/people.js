b21c064cbfdb787d17c49ba947456db8cca6baf8
Melih Onvural <melih.onvural@gmail.com>
2012-05-01 14:16:40 -0700

    Reworked the view of comments, shares, + feeds. People/Share stacks work now

---
+++ public/javascripts/people.js
@@ -0,0 +1,18 @@
+var loadManagePeople = function() {
+	alert("Coming soon!");
+
+	// $.ajax({
+		// url: "/follows/manage",
+		// success: function (data, textStatus, jqXHR) {
+			// $("#amplifizeContent").html(data);
+		// },
+		// error: function(xhr, text, error) {
+			// alert(error);
+		// },
+		// dataType: "html"
+	// });
+};
+
+$(document).ready(function() {
+	$("li#peopleNav.drawer ul").css("display", "block").css("visibility", "visible");
+});
--- public/javascripts/shares.js
+++ public/javascripts/shares.js
@@ -1,9 +1,6 @@
-$(document).ready(function() {
-	updateShareContent(shares[position]); 
-});
-
 var current_share = undefined;
 var position = 0;
+var max_position = 0;
 
 var setReadState = function(readState) {
 	$.ajax({
@@ -18,20 +15,31 @@ var setReadState = function(readState) {
 	
 	return false;
 }
-	
+
 var downPost = function() {
 	if(position > 0) {
 		position--;
-		updateShareContent(posts[position]);
+		updateShareContent(shares[position]);
+	} else {
+		alert("At the first share");
 	}
 	
 	return false;
 };
 	
 var upPost = function() {
-	if(position < posts.length) {
+	if((position+1) < posts.length) {
 		position++;
-		updateShareContent(posts[position]);
+		updateShareContent(shares[position]);
+		
+		if(position > max_position) {
+			max_position = position;
+			var unread_count = posts.length - 1 - max_position;
+			$("#shareUnreadCount").html(unread_count);
+			document.title = "Amplifize | Give good content a voice ("+unread_count+")"
+		}
+	} else {
+		alert("No more posts");
 	}
 	
 	return false;
@@ -48,7 +56,7 @@ var updateShareContent = function(shareId) {
 			
 			$("#contentTitle").html('<a href="'+current_post.url+'" target="_blank">'+current_post.title+'</a></p>');
 			$("#contentPublishDate").html(dateFormat(current_post.published_at, "dddd, mmmm dS, yyyy, h:MM:ss TT"));
-			$("#shareSummary").html(data.share.summary);
+			$("#contentSummary").html(data.share.summary);
 
 			for(var i = 0; i < data.share.comments.length; i++) {
 				var comment = data.share.comments[i];
@@ -65,3 +73,31 @@ var updateShareContent = function(shareId) {
 	})
 };
 
+$(document).ready(function() {
+	if(shares.length > 0) {
+		//need to do this to prevent firefox from auto searching on typing
+		jQuery(document).bind('keydown', 'j', function(evt) {
+			return false;
+		});
+
+		jQuery(document).bind('keydown', 'k', function(evt) {
+			return false;
+		});
+		
+		jQuery(document).bind('keyup', 'j', function(evt) {
+			 upPost();
+			 return false;
+		});
+
+		jQuery(document).bind('keyup', 'k', function(evt) {
+			downPost();
+			return false;
+		});
+
+		var unread_count = shares.length - 1; 
+		$("#shareUnreadCount").html(unread_count);
+		document.title = "Amplifize | Give good content a voice ("+unread_count+")";
+	}
+
+	updateShareContent(shares[position]); 
+});
