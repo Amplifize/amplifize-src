8701d592ae84787d79f2e5eac2ce579167d531cd
Melih Onvural <melih.onvural@gmail.com>
2012-05-01 11:13:04 -0700

    Clean up the follow user flow. Now gives feedback

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,7 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-//$.ajaxSettings.dataType = "json";
+$.ajaxSettings.dataType = "json";
 
 $(document).ready(function() {
 
--- public/javascripts/findPeople.js
+++ public/javascripts/findPeople.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
 	$('form#userSearchForm').bind("ajax:success", function(status, data, xhr) {
-		$("#search_email").val("");
-		//TODO: if data is blank, then say so
-		//else
-		$('#userSearchResultsTable > tbody:last').append('<tr><td>'+data.user.email+'</td><td><a href="#" onclick="followUser('+data.user.id+')">Follow</a></td></tr>');
+		var formField = $("#search_email");
+		if(null == data) {
+			$("#emptyUserSearchResult").html("No amplifize user found with email address: "+formField.val());
+		}
+		else {
+			$('#userSearchResultsTable > tbody:last').append('<tr><td>'+data.user.email+'</td><td id="follow_'+data.user.id+'"><a href="#" onclick="followUser('+data.user.id+')">Follow</a></td></tr>');
+		}
+
+		formField.val("");
 	});
 
 	$('form#userSearchForm').bind("ajax:failure", function(xhr, status, error) {
@@ -15,7 +20,7 @@ var followUser = function(userId) {
 	$.ajax({
 		url: '/follows/add/'+userId,
 		success: function(data, textStatus, jqXHR) {
-			alert("It worked. Click okay!");
+			$("#follow_"+userId).html("Successfully followed");
 		},
 		error: function(xhr, text, error) {
 			alert(error);
