bbce35a94bd7ac6db6f0a341bdb3b08418622640
Matthew Davis <mattd@ou.edu>
2012-05-21 22:24:49 -0700

    update to parallel feeds

--- lib/feed_updater.rb
+++ lib/feed_updater.rb
@@ -7,13 +7,28 @@ class FeedUpdater
   end
 
   def self.queue_feed_update(feed)
-    begin
-      job = FeedUpdaterJob.new
-      job.delay.run_update(feed.id)
+    #begin
+      # Using the "delay" syntax queues to delayed_job
+      FeedUpdater.delay.run_update(feed.id)
       #@@sender.rpush("feed_upates", feed.id)
-    rescue
-      Post.get_new_posts(feed)
-      Post.synchronize_posts_with_users(current_user.id, feed.id)
+    #rescue
+    #  Post.get_new_posts(feed)
+    #  #Post.synchronize_posts_with_users(current_user.id, feed.id)
+    #end
+  end
+  
+  
+  def self.run_update(feed_id)
+    thread_num = 0
+    puts ">> Updating feed #{feed_id}"
+    t = Timer.new
+    begin
+      FeedUpdater.update_feed(feed_id)
+    rescue Exception => e
+      puts "!! Error with feed #{feed_id}: #{e} : #{t.elapsed}ms"
+    next
     end
+
+    puts "<< Done with feed #{feed_id} : #{t.elapsed}ms"
   end
 end
--- lib/feeds_job.rb
+++ lib/feeds_job.rb
@@ -5,18 +5,4 @@ class FeedsJob
       FeedUpdater.queue_feed_update(feed)
     }
   end
-
-  def run_update(feed_id)
-    thread_num = 0
-    puts ">> [#{thread_num}] Updating feed #{feed_id}"
-    t = Timer.new
-    begin
-      FeedUpdater.update_feed(feed_id)
-    rescue Exception => e
-      puts "!! [#{thread_num}] Error with feed #{feed_id}: #{e} : #{t.elapsed}ms"
-    next
-    end
-
-    puts "<< [#{thread_num}] Done with feed #{feed_id} : #{t.elapsed}ms"
-  end
 end
