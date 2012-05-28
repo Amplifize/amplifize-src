3def3cd928d7873e0cfc7bb8d5e41a2dab95d1df
Melih Onvural <melih.onvural@gmail.com>
2012-05-27 23:59:22 -0700

    Add the meaning of status integers for post_user Add the meaning of status integers for share_user

--- app/models/post_user.rb
+++ app/models/post_user.rb
@@ -1,4 +1,8 @@
 class PostUser < ActiveRecord::Base
+  # read_state status codes are as follows:
+  # => 0 == read
+  # => 1 == unread
+
   belongs_to :post
   belongs_to :user
 end
--- app/models/share_user.rb
+++ app/models/share_user.rb
@@ -1,4 +1,8 @@
 class ShareUser < ActiveRecord::Base
+  # read_state status codes are as follows:
+  # => 0 == read
+  # => 1 == unread
+
   belongs_to :share
   belongs_to :user
 end
