b4e74a891f96490ab440ea382c78523c629160f4
Melih Onvural <melih.onvural@gmail.com>
2012-09-03 23:33:35 -0700

    All links internal to a post open in a new window

--- public/javascripts/reader.js
+++ public/javascripts/reader.js
@@ -6,6 +6,11 @@ var disableOverlay = function() {
 	$("#overlay").css("visibility", "hidden");
 };
 
-$(function () { 
-	
+$(document).ready(function() {
+	$("#contentSummary").on("click", "a", function(evt) {
+		if($(this).attr("href").indexOf("http") != -1) {
+			evt.preventDefault();
+			window.open($(this).attr("href"));
+		}
+	});
 });
