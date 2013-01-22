1e1efe35e01ec0d12f2df0ca7a3b95797fec4bc2
Melih Onvural <melih.onvural@gmail.com>
2013-01-21 20:08:09 -0800

    Get the static pages to work with the new layout Setup the global javascript/css properties

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require global.js
+//= require global
 
 
 /* require_tree . */
--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -43,7 +43,6 @@ class UserSessionsController < ApplicationController
 
     respond_to do |format|
       format.html # new.html.erb
-      format.xml  { render :xml => @user_session }
     end
   end
 
@@ -54,14 +53,9 @@ class UserSessionsController < ApplicationController
 
     respond_to do |format|
       if @user_session.save        
-        session[ :read_state] = "unread"
-        session[ :read_order] = "oldToNew"
-        
         format.html { redirect_back_or_default(:homepage, 'Login Successful') }
-        format.xml  { render :xml => @user_session, :status => :created, :location => @user_session }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @user_session.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -76,7 +70,6 @@ class UserSessionsController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to(:root, :notice => 'Goodbye!') }
-      format.xml  { head :ok }
     end
   end
   
