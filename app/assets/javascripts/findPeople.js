7e262ba153d71f5d90398c8042f1ea3159d89bb0
Melih Onvural <melih.onvural@gmail.com>
2013-03-21 23:57:33 -0700

    Revert "Fixed a bug where searching for people didn't work"
    This reverts commit 23cab37203fe8084eef3e6629d4bfb91c7d18d01.

--- app/assets/javascripts/findPeople.js
+++ app/assets/javascripts/findPeople.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 			$("#emptyUserSearchResult").html("No amplifize user found with email address: "+formField.val() +".<br /><a href=\"#\" data-dismiss=\"modal\" onclick=\"openInviteModal('"+formField.val()+"')\">Why not invite them?</a>");
 		}
 		else {
-			$('#userSearchResultsTable > tbody:last').append('<tr><td>'+data.email+'</td><td id="follow_'+data.id+'"><a href="#" onclick="followUser('+data.id+')">Follow</a></td></tr>');
+			$('#userSearchResultsTable > tbody:last').append('<tr><td>'+data.user.email+'</td><td id="follow_'+data.user.id+'"><a href="#" onclick="followUser('+data.user.id+')">Follow</a></td></tr>');
 		}
 
 		formField.val("");
