c5d2189c38e32b8c23dcc87a8765354d3780ccab
Melih Onvural <melih.onvural@gmail.com>
2013-06-28 00:10:54 -0700

    Fixed a bug in the scheduler

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
 every 10.minutes do
     rake "update_feeds"
-    run "RAILS_ENV=production script/delayed_job -n 10 --queue=mail,feed_update start"
+    command "RAILS_ENV=production script/delayed_job -n 10 --queue=mail,feed_update start"
 end
