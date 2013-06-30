7bcae867c29004fec54813dc0dd7f2ac61354d91
Melih Onvural <melih.onvural@gmail.com>
2013-06-29 21:18:40 -0700

    Updated the schedule to move the directory before it executes the delayed_job command

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
 every 10.minutes do
     rake "update_feeds"
-    command "RAILS_ENV=production script/delayed_job -n 10 --queue=mail,feed_update start"
+    command "cd /home/amplifize.com/src && RAILS_ENV=production script/delayed_job -n 10 --queue=mail,feed_update start"
 end
