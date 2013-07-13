107723c806775e2cd75d803a4b2cb068f435ef61
Melih Onvural <melih.onvural@gmail.com>
2013-07-12 20:27:31 -0700

    New crontab

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,5 @@
 every 10.minutes do
     rake "update_feeds"
-    command "cd /home/amplifize.com/src && RAILS_ENV=production script/delayed_job -n 10 --queue=mail,feed_update start"
+    #command "cd /home/amplifize.com/src && RAILS_ENV=production script/delayed_job -n 10 --queue=mail,feed_update start"
+    command "cd /home/amplifize.com/src && RAILS_ENV=production script/delayed_job start --exit-on-complete"
 end
